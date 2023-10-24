24d471f54d8c1af39f478d6abcf70714b46bbca7
kyohei-dan <kyohey.dan@gmail.com>
2023-10-24 16:12:51 +0900

    [Fix] コンソールに出力するメッセージとスクリプトを調整

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,8 +1,34 @@
 export const functions = {
   // ▼コンソール出力用
   setConsoleText: () => {
-    const text = ["\n %c Developed by Kyohei Dan. -> https://next-notion-blog-kyohei-dan.vercel.app \n\n", "background: #0000FF; padding:5px 0; color: #ffffff;"];
-    window.console.log.apply(console, text);
+    const taglineOptions = ["Build Fast Web Experiences.", "Create an Accessible Web."];
+    const tagline = taglineOptions[Math.floor(Math.random() * taglineOptions.length)];
+
+    let lineHeight = 32;
+    if (navigator.userAgent.includes("Firefox")) {
+      lineHeight = 14;
+    }
+
+    const message = {
+      text: `%c${tagline}`,
+      styles: [`color: inherit; font-size: 20px; font-weight: 900; margin-bottom: 12px; line-height: 24px;`],
+      logo: {
+        text: "%c/ %c",
+        styles: [`font-size: 24px; margin-right: 0px; line-height: ${lineHeight}px;`, ""],
+      },
+      line: {
+        text: `%c\nDeveloped by Kyohei Dan -> https://next-notion-blog-kyohei-dan.vercel.app\n %c`,
+        styles: [`color: inherit; font-size: 14px;`, ""],
+      },
+    };
+
+    let logoStyles;
+    let textStyles;
+    message.text = `${message.logo.text}${message.text}${message.line.text}`;
+    (logoStyles = message.styles).unshift.apply(logoStyles, message.logo.styles);
+    (textStyles = message.styles).push.apply(textStyles, message.line.styles);
+
+    console.log.apply(this, [`\n${message.text}`].concat(message.styles));
   },
 
   // ▼360px 未満は JS で viewport を固定する
